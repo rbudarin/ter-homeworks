f52ca5c12445d409f2b622c43bf126abc5fab751
Roman <r.bud4rin@yandex.ru>
2024-12-30 20:06:54 +0300

    for_each-vm.tf

--- 03/src/for_each-vm.tf
+++ 03/src/for_each-vm.tf
@@ -1,28 +1,22 @@
-# resource "random_password" "each" {
-#   for_each    = toset([for k, v in yandex_compute_instance.example : v.name ])
-#   length = 17
-# #> type(random_password.each) object(object)
-# }
-
-# VM_WEB (2)
+# VM_DB (2)
 resource "yandex_compute_instance" "vm_db" {
-  for_each    = toset([ 0, 1 ])
-  
-  name        = "web-${each.key}"
-  hostname    = "web-${each.key}"
+  depends_on = [ yandex_compute_instance.web ]
+  for_each = { for vm in var.each_vm : "${vm.vm_name}" => vm }
+  name        = "db-${each.key}"
+  hostname    = "db-${each.key}"
   platform_id = var.web_platform
   
   resources {
-    cores         = var.vms_resources[0].cores
-    memory        = var.vms_resources[0].memory
-    core_fraction = var.vms_resources[0].core_fraction
+    cores         = each.value.cpu
+    memory        = each.value.ram
+    core_fraction = each.value.core_fraction
   }
 
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.image_id
-      type     = var.vm_web_hdd[0].type
-      size     = var.vm_web_hdd[0].size
+      type     = each.value.disk_type
+      size     = each.value.disk_volume
     }
   }
 
